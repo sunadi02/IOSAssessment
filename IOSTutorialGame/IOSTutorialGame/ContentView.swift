2a6db3554eabbb6d07ac5152c20d459d81a03bec
Sunadi Amarasinghe <sunadiamarasinghe@gmail.com>
2026-06-10 16:16:51 +0530

    Initial Xcode SwiftUI project setup

---
+++ IOSTutorialGame/IOSTutorialGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSTutorialGame
+//
+//  Created by student2 on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSTutorialGame/IOSTutorialGame/IOSTutorialGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTutorialGameApp.swift
+//  IOSTutorialGame
+//
+//  Created by student2 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct IOSTutorialGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
